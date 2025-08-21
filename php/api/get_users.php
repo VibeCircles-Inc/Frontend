fc95817e010d2fe1428872e5eddc5f4d757a3dce
VibesCircles <vibecircless@gmail.com>
2025-08-21 21:57:20 +0200

    changes made

--- php/api/get_users.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../config/db.php';
-
-$stmt = $pdo->query('SELECT * FROM users');
-$users = $stmt->fetchAll();
-echo json_encode($users);
-?>
--- php/auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// php/auth/logout.php
-header('Content-Type: application/json');
-session_start();
-session_unset();
-session_destroy();
-echo json_encode(['success' => true, 'message' => 'Logged out successfully.']); 
